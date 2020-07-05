8af7b9f3fe6aa2bf22906c5ccb0da4f8b8211486
zaraki <rohansingh94117@gmail.com>
2020-07-06 04:10:22 +0530

    Added SwipeRefreshLayout dependency Completed Whole UI

---
+++ app/src/main/java/com/example/newsapp/ui/HeadlinesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsapp.R
+
+class HeadlinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_headlines)
+    }
+}
--- app/src/main/java/com/example/newsapp/ui/NewsDetailsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsDetailsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.newsapp
+package com.example.newsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news_details)
     }
 }
